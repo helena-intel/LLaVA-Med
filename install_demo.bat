86ae5c678774666f797420af34ddcc754a33964a
Helena Kloosterman <helena.kloosterman@intel.com>
2026-04-08 13:46:54 +0200

    Update install_demo.bat

--- install_demo.bat
+++ install_demo.bat
@@ -3,7 +3,7 @@ cd llavamed_demo
 python -m venv llava_env
 llava_env\Scripts\python -m pip install --upgrade pip
 llava_env\Scripts\python -m pip install git+https://github.com/helena-intel/Llava-Med.git
-llava_env\Scripts\huggingface-cli download helenai/llava-med-imf16-llmint8 --local-dir llava-med-imf16-llmint8
+llava_env\Scripts\hf download helenai/llava-med-imf16-llmint8 --local-dir llava-med-imf16-llmint8
 git clone https://github.com/LiangXin1001/LLaVA_Med_qa50_images.git data
 tar -xf data\qa50_images.zip
 move qa50_images data
@@ -11,7 +11,7 @@ curl -O https://raw.githubusercontent.com/helena-intel/LLaVA-Med/821e1c7e34a458e
 mkdir data\eval
 move llava_med_eval_qa50_qa.jsonl data\eval
 
-echo llava_env\Scripts\huggingface-cli download helenai/llava-med-imf16-llmint8 --local-dir llava-med-imf16-llmint8 > update.bat
+echo llava_env\Scripts\hf download helenai/llava-med-imf16-llmint8 --local-dir llava-med-imf16-llmint8 > update.bat
 echo curl -O  https://raw.githubusercontent.com/helena-intel/LLaVA-Med/refs/heads/main/llavamed_inference_openvino.ipynb >> update.bat
 echo curl -O  https://raw.githubusercontent.com/helena-intel/LLaVA-Med/refs/heads/main/llavamed_inference_openvino.py >> update.bat
 echo curl -O  https://raw.githubusercontent.com/helena-intel/LLaVA-Med/refs/heads/main/app.py >> update.bat
